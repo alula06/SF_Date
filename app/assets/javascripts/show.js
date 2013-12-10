ddf6660409631fc16dee81b88432d963e7c1c7d1
A.Beshue <alula06@yahoo.com>
2013-12-09 17:12:58 -0800

    Updated logo and calendar css

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -5,6 +5,7 @@ var day = current.getDay();
 var date = current.getDate();
 var year = current.getFullYear();
 
+
 // array of month names 
 var monthNames = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov", "Dec"];
 
@@ -31,7 +32,8 @@ function init_Calendar(){
     $("#body").append($("<tr>"));
 
     //Event Feed body
-    $("#eventbody").append("<div>");
+    $("#eventbody").append("<h3>Event Feed</h3><div><table>");
+
     
     var first_Day = get_First_Day(month, year);
     //Create empty columns before first day of month
@@ -42,7 +44,7 @@ function init_Calendar(){
     var date_Num = 1;
     //Start creating columns beginning with first day
     for (var i = 1; i <= 7 - first_Day; i++) {
-        $("#body tr:last").append("<td data-day='" + i + "'>" + "<div class = 'datenum'>" + date_Num.toString() + "</div>" + "</td>");  
+        $("#body tr:last").append("<td data-day='" + i + "' class = 'datenum'>" + date_Num.toString() + "</td>");  
         //Highlight column if it the day current day   
         if (date_Num == date && month == current.getMonth() && year == current.getFullYear()) {
             $("#body td:last").css("cssText", "background-color: #eee");
@@ -60,7 +62,7 @@ function init_Calendar(){
                 $("#body tr:last").append("<td>" + "</td>");
             } else {
                 //If not assign day number to column
-                $("#body tr:last").append("<td data-day='" + date_Num+ "'>" + "<div class = 'datenum'>" + date_Num.toString() + "</div>" + "</td><br>");
+                $("#body tr:last").append("<td data-day='" + date_Num+ "'class = 'datenum'>" + date_Num.toString() + "</td>");
             }
             //Highlight column if it the day current day 
             if (date_Num == date && month == current.getMonth() && year == current.getFullYear()) {
@@ -79,7 +81,7 @@ function init_Calendar(){
             var temp = cell + " " + input + " at " + timer;
             $(this).text(temp);
             $.post( "/events/create", { 'apps':
-                                            {"event":input,
+                                            {"events":input,
                                             "time":timer,
                                             "day": $(this).data('day'),
                                             "month": month,
@@ -107,28 +109,28 @@ function init_Calendar(){
     });
 
     
-
     
     $.get( "/events/index", { "month": month,"year": year },
                              function(data){
-                                                
+                                             
                                                 $.each(data, function(key, value){
-                                            
-                                                    $("td[data-day='" + value.day + "']").append("<li>" + value.event + " at " + value.time +"</li>");
-                                                     $("#eventbody div:last").append("<div class = 'eventpost'>").append(++value.month + "-" + value.day + "-" + value.year + " " + value.event + " to start at " + value.time + " created on " + value.created_at );
                                                     
-
-                                                     
+                                                    $("td[data-day='" + value.day + "']").append("<p class = 'tddesc'>Event: " + value.event + "</p><p class = 'tddesc'>Time: " + value.time + "</p><p class ='tddesc'>Location: " + value.location + "</p><br>");
+                                                     $("#eventbody table:last").append("<tr><td id = 'eventdate'>"+ ++value.month + "-" + value.day + "-" + value.year + "</td><td = 'eventdesc'>" + value.event + " to start at " + value.time + " created on " + value.created_at + "</td></tr>"); 
+                 
 
                                                 });
                                             });
+     $("#eventbody").append("</table>");
+
+
 
    
     
 
 // $.get( "/events/show_user", function(data){
                                                     // $(".eventpost div:last").append("posted by " + data.name);
-                                                     //    });
+                                                        // });
     
    
 }
