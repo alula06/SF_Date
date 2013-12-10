af15d7d9c8d56f508f58d82045f334f963aabbf8
A.Beshue <alula06@yahoo.com>
2013-12-09 17:24:08 -0800

    Added description to users table and location to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204233307) do
+ActiveRecord::Schema.define(version: 20131210012136) do
 
   create_table "events", force: true do |t|
     t.string   "event"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20131204233307) do
     t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
   end
 
   create_table "microposts", force: true do |t|
@@ -53,6 +54,7 @@ ActiveRecord::Schema.define(version: 20131204233307) do
     t.boolean  "admin",           default: false
     t.string   "hometown"
     t.string   "age"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
